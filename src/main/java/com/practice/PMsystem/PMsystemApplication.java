1fc80e28b43d3ce8c70e61cf89036a790f657d09
Aleksander Witek <aleksanderwitek95@gmail.com>
2024-09-11 16:28:01 +0100

    Prepare db schema and dependencies for new project

---
+++ src/main/java/com/practice/PMsystem/PMsystemApplication.java
@@ -0,0 +1,13 @@
+package com.practice.PMsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PMsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PMsystemApplication.class, args);
+	}
+
+}
